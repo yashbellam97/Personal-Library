a500044692e042322e3973050a818b4db485e01a
yashbellam97 <yashbellam97@gmail.com>
2020-08-20 22:49:42 -0400

    Add a header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import BooksPage from "./pages/BooksPage";
+import Header from "./components/Header"
 
 function App() {
     return (
         <div>
+            <Header />
             <BooksPage />
         </div>
     );
