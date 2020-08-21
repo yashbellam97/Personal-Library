8f1085a40ee93b06f553178b063f7b753a811833
yashbellam97 <yashbellam97@gmail.com>
2020-08-21 11:58:40 -0400

    Add background and footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from "react";
 import "./index.css"
 
 import BooksPage from "./pages/BooksPage";
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <Header />
             <BooksPage />
+            <Footer />
         </div>
     );
 }
