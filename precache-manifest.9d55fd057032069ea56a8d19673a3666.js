70a911dd7bfed33443034f0f04b479c69d30d29e
yashbellam97 <yashbellam97@gmail.com>
2020-08-21 22:33:14 -0400

    Updates

---
+++ precache-manifest.9d55fd057032069ea56a8d19673a3666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2accb705e97a4fd21c1873257b277cb1",
+    "url": "/Personal-Library/index.html"
+  },
+  {
+    "revision": "7d335ebf1e711d9ff9f6",
+    "url": "/Personal-Library/static/css/main.d20ca8f5.chunk.css"
+  },
+  {
+    "revision": "b34547fade6705f230a1",
+    "url": "/Personal-Library/static/js/2.d3f50c85.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Personal-Library/static/js/2.d3f50c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d335ebf1e711d9ff9f6",
+    "url": "/Personal-Library/static/js/main.c8b0e28c.chunk.js"
+  },
+  {
+    "revision": "fd13bdc9af0ac255fe63",
+    "url": "/Personal-Library/static/js/runtime-main.5e42adfd.js"
+  }
+]);
