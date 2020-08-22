692efbeb57f04216253df606f628308638b7f265
yashbellam97 <yashbellam97@gmail.com>
2020-08-21 22:30:52 -0400

    Add logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+import bookLogo from "../images/books.png";
+import githubLogo from "../images/github.png";
+
 function Header() {
     return (
         <div className="header">
-            <h1>Personal Library</h1>
+            <img src={bookLogo} style={{filter: "invert(1)", marginRight: "20px", verticalAlign: "middle"}} alt="A book logo" height="80" width="80"></img>
+            <a href="https://yashbellam97.github.io/Personal-Library/" target="blank">
+                <img src={githubLogo} style={{float:"right", margin: "20px 0", verticalAlign: "middle"}} alt="GitHub logo" height="50" width="50"></img>
+            </a>
+            <h1 style={{display: "inline", verticalAlign: "middle", userSelect: "none"}}>Personal Library</h1>
         </div>
     );
 }
